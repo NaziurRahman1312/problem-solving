92879f548744baed2d212532f9e968ecc7b258d4
Md Naziur Rahman <45492415+NaziurRahman1312@users.noreply.github.com>
2024-01-24 17:14:37 +0600

    66. Plus One

--- LeetCode/66. Plus One/Solution.js
+++ LeetCode/66. Plus One/Solution.js
@@ -1,48 +1,18 @@
 /**
- * @param {string} a
- * @param {string} b
- * @return {string}
+ * @param {number[]} digits
+ * @return {number[]}
  */
-var addBinary = function (a, b) {
-  let carry = 0;
-  let alen = a.length - 1;
-  let blen = b.length - 1;
-  let result = "";
+var plusOne = function(digits) {
+  let carry = 1;
 
-  while (alen >= 0 || blen >= 0) {
-    if (alen >= 0 && blen >= 0) {
-      carry += a[alen] - "0" + (b[blen] - "0");
-
-      if (carry === 3) {
-        result = "1".concat(result);
-        carry = 1;
-      } else if (carry === 2) {
-        result = "0".concat(result);
-        carry = 1;
-      } else {
-        result = (carry === 1 ? "1" : "0").concat(result);
-        carry = 0;
-      }
-    } else {
-      carry += alen >= 0 ? a[alen] - "0" : b[blen] - "0";
-
-      if (carry === 3) {
-        result = "1".concat(result);
-        carry = 1;
-      } else if (carry === 2) {
-        result = "0".concat(result);
-        carry = 1;
-      } else {
-        result = (carry === 1 ? "1" : "0").concat(result);
-        carry = 0;
-      }
-    }
-
-    alen--;
-    blen--;
+  for(let i = digits.length - 1; i >= 0; i--){
+      if(carry === 0) return digits;
+      let val = carry + digits[i]
+      digits[i] = val === 10 ? 0 : val;
+      carry = val === 10 ? 1 : 0;
   }
 
-  result = (carry === 1 ? "1" : "").concat(result);
-
-  return result;
+  if(carry === 1){
+     return [1].concat(digits);
+  }else return digits;
 };
